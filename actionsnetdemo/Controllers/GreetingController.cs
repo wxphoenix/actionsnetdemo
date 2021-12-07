dfffeb400ca5bc6cce0a4894640b4031758b1453
许阳 <mksword@126.com>
2021-12-07 20:52:14 +0800

    Greeting

---
+++ actionsnetdemo/Controllers/GreetingController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace actionsnetdemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GreetingController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello World!";
+        }
+    }
+}
